78418c070ba9afd4c41db81b66cbe920c7712660
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-19 10:14:00 -0700

    sub_code.c

---
+++ sub_code.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * sub - subtracts the top element of the stack.
+ * @h_stack: the head of the stack.
+ * @line_number: Line number in the input file.
+ *
+ * Return: void.
+ */
+void sub(stack_t **h_stack, unsigned int line_number)
+{
+	stack_t *o_head;
+	unsigned int length = 0, tmp;
+
+	o_head = *h_stack;
+
+	while (o_head != NULL)
+	{
+		length++;
+		o_head = o_head->next;
+	}
+	if (length < 2)
+	{
+		fflush(stdout);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fclose(global.file);
+		free(global.line);
+		f_stack(*h_stack);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*h_stack)->next->n - (*h_stack)->n;
+	(*h_stack)->next->n = tmp;
+	o_head = *h_stack;
+	(*h_stack) = (*h_stack)->next;
+	free(o_head);
+}
