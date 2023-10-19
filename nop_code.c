a52eb821f7d3063ba867e91aeffceab3018f4fc2
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-19 10:13:35 -0700

    nop_code.c

---
+++ nop_code.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - do nothing
+ * @h_stack: the head of the stack
+ * @line_number: line number
+ * Return: void.
+ */
+void nop(stack_t **h_stack, unsigned int line_number)
+{
+	(void) h_stack;
+	(void) line_number;
+}
